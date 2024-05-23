eda0ca09154e4d1bbe75245e9e4c91290c3afc85
Yuvraj Rathi <yrathi66@gmail.com>
2024-05-23 12:13:13 +0530

    ugh

--- myapi.py
+++ myapi.py
@@ -1,6 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
+
+students = {
+    1: {
+        "name": "John",
+        "age": 17,
+        "class": "year 12"
+    },
+    2: {
+        "name": "Alice",
+        "age": 16,
+        "class": "year 11"
+    },
+    3: {
+        "name": "Bob",
+        "age": 16,
+        "class": "year 11"
+    }
+}
 
 # amzn.com/create-user
 # here create-user is a path parameter or an endpoint in terms of FastAPI
@@ -22,5 +40,6 @@ def index():
 # visit 127.0.0.1:8000/docs in the browser for swagger ui, cool stuff -> no need to use bruno or postman
 # removes the use of third party api testing applications
 
- 
-
+@app.get("/get-student/{student_id}") # path parameter
+def get_student(student_id: int = Path(None, description="Enter the student ID", gt=0, lt=3)):
+    return students[student_id]
